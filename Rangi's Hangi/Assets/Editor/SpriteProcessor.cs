65ba6e72c774417fe4e2acf95486f4e99e2e5052
EasternCatNZL <brianpwwong@gmail.com>
2018-08-08 14:44:17 +1200

    Added editor script that defaults pngs to sprites instead of textures

---
+++ Rangi's Hangi/Assets/Editor/SpriteProcessor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class SpriteProcessor : AssetPostprocessor
+{
+    //Immediatly changes pngs to sprites instead of textures
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        string lowerCaseAssetPath = assetPath.ToLower();
+        bool isInSpritesDirectory = lowerCaseAssetPath.IndexOf("/sprites/") != -1;
+
+        if (isInSpritesDirectory)
+        {
+            TextureImporter textureImporter = (TextureImporter)assetImporter;
+            textureImporter.textureType = TextureImporterType.Sprite;
+        }
+    }
+}

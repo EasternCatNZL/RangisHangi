743462b2ab2153d8140287ac41648493e97a8827
EasternCatNZL <brianpwwong@gmail.com>
2018-08-17 19:33:29 +1200

    Timer

---
+++ Rangi's Hangi/Assets/Scripts/Handlers/GameInstanceHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstanceHandler : MonoBehaviour {
+
+    public ProblemGenerator problemGenerator;
+    public Timer timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void EndRound()
+    {
+
+    }
+
+    public void EndGame()
+    {
+
+    }
+}
---
+++ Rangi's Hangi/Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    [Header("Timer stuff")]
+    public float totalTime = 10.0f;
+
+    [Header("UI stuff")]
+    public Text timerText;
+
+    bool isActive = false;
+    float timeLeft = 0.0f;
+    float timeTimerStarted = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        StartTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isActive)
+        {
+            ClockTick();
+        }
+        
+	}
+
+    public void StartTimer()
+    {
+        timeLeft = totalTime;
+        timeTimerStarted = Time.time;
+        isActive = true;
+    }
+
+    public void StopTimer()
+    {
+        timeLeft = 0.0f;
+        isActive = false;
+        //anything else that needs to happen when timer stops
+
+    }
+
+    void ClockTick()
+    {
+        timeLeft = totalTime - (Time.time - timeTimerStarted);
+        if(timeLeft <= 0)
+        {
+            StopTimer();
+        }
+        PresentTime();
+    }
+
+    void PresentTime()
+    {
+        int secondsLeft = (int)(timeLeft % 60);
+        float timeMinusSeconds = timeLeft - secondsLeft;
+        int minutesLeft = (int)(timeMinusSeconds / 60);
+        timerText.text = minutesLeft + ":" + secondsLeft;
+    }
+}
